2a10b3406448a320007c416587ce600f11081b3f
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-01-09 16:00:55 +0000

    created mart for custom audience fb

---
+++ models/marts/marketing/fb_custom_audience_list.sql
@@ -0,0 +1,15 @@
+with 
+int_fb_custom_audience_list as
+(
+select* 
+from 
+{{ ref('int_fb_custom_audience_list') }}
+),
+
+fb_custom_audience_list as 
+(
+    select *
+    from int_fb_custom_audience_list
+)
+
+select * from fb_custom_audience_list
