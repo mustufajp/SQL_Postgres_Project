4b32ff29ce57ebc372f03913a32a104989078d66
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-01-09 06:27:40 +0000

    changed the format to fit with the dbt convention

--- models/intermediate/products/int_sales_aggregated_to_product.sql
+++ models/intermediate/products/int_sales_aggregated_to_product.sql
@@ -1,20 +1,11 @@
-WITH 
-sku_transactions as 
-(
-    SELECT *
-    FROM {{ ref('stg_saad_shop__sku_transactions') }}
-)
-,skus as 
-(
-    SELECT *
-    FROM {{ ref('stg_saad_shop__skus') }}
-)
+with
+    sku_transactions as (select * from {{ ref("stg_saad_shop__sku_transactions") }}),
+    skus as (select * from {{ ref("stg_saad_shop__skus") }}),
+    int_sales_aggregated_to_product as (
+        select *
+        from sku_transactions
+        left join skus using (sku_id)
+        order by transaction_id
+    )
 
-SELECT 
-*
-FROM
- sku_transactions
-LEFT JOIN
- skus
-using (sku_id)
-ORDER BY transaction_id
+select * FROM int_sales_aggregated_to_product
