69792b3622ed52898e4ac0beb0a7bd82d3ceeecc
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-01-09 03:26:34 +0000

    stg for sku transaction

---
+++ models/staging/saad_shop/stg_saad_shop__sku_transactions.sql
@@ -0,0 +1,30 @@
+with 
+
+source as (
+
+    select * from {{ source('saad_shop', 'sku_transactions') }}
+
+),
+
+renamed as (
+
+    select
+        transaction_id,
+        custom_discount->>'coupon' AS product_custom_discount_coupon,
+        custom_discount->>'price' AS product_custom_discount_price,
+        sku_id,
+        quantity,
+        price_at_purchase,
+        key,
+        discounted_amount as product_discounted_amount,
+        id as sku_transaction_id,
+        point_back_amount as product_point_back_amount,
+        point_spent_per as product_point_spent_per,
+        custom_point_back_rate,
+        point_back_rate
+
+    from source
+
+)
+
+select * from renamed
