4c88a7602f02e650b4e074570952cdce3487ba2d
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-01-09 03:09:04 +0000

    Created stg for sales

---
+++ models/staging/saad_shop/stg_saad_shop__sales.sql
@@ -0,0 +1,33 @@
+with 
+
+source as (
+
+    select * from {{ source('saad_shop', 'sales') }}
+
+),
+
+renamed as (
+
+    select
+        id AS sales_id,
+        transaction_id,
+        amount AS sales_amount,
+        type AS sales_type,
+        employee_id,
+        store_id AS sales_store_id,
+        points_given,
+        points_used,
+        total_discount,
+        estimated_age AS customer_age,
+        gender AS customer_gender,
+        COALESCE(REPLACE((methods->0->>'type')::text, '"', ''), '') AS customer_payment_type,
+        COALESCE(REPLACE((methods->0->>'method')::text, '"', ''), '') AS customer_payment_method,
+        created_at AS sales_date,
+        affiliate_commission_point,
+        affiliate_commission_amount,
+        (custom_discount->>'price')::numeric AS custom_discount_price,
+        (custom_discount->>'coupon')::numeric AS custom_discount_coupon
+    from source
+)
+
+select * from renamed
