28d3df4a07fcc1cb87bce225fc12518eaafe1cc9
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-01-10 16:23:09 +0000

    moved files and changed column names for userinfo to fix dbt run fail

--- models/intermediate/marketing/customer_info/int_add_addresses_to_user_info.sql
+++ models/intermediate/marketing/customer_info/int_add_addresses_to_user_info.sql
@@ -2,13 +2,43 @@ with
 
 user_info as (
 
-    select *
+    select
+        user_id as customer_id,
+        user_phone_number,
+        user_status,
+        user_type,
+        is_user_email_verified,
+        user_email,
+        is_user_verified,
+        user_kyc_status,
+        user_created_at,
+        user_updated_at,
+        is_magazine_subscribed,
+        is_agree_to_term_of_service,
+        is_user_profile_verified,
+        first_name,
+        last_name,
+        gender,
+        date_of_birth,
+        address_id,
+        user_profile_updated_at
     from {{ ref('int_user_profile_joined_to_users') }}
     where user_type='customer'
 ),
 addresses as (
     select
-    *
+        address_id,
+        post_code,
+        prefecture,
+        city,
+        town,
+        line_one,
+        line_two,
+        full_width_kana,
+        latitude,
+        longitude,
+        country,
+        combined_address
     from {{ ref('stg_saad_shop__addresses') }}
 ),
 
@@ -20,3 +50,4 @@ int_add_addresses_to_user_info as (
 )
 
 select * from int_add_addresses_to_user_info
+
