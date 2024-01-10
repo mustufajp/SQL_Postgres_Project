e1dd50f95c4ef19f9c7da368fe049fb9bb0bcd0b
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-01-10 07:21:45 +0000

    conform to dbt best practice

--- models/staging/saad_shop/stg_saad_shop__stores.sql
+++ models/staging/saad_shop/stg_saad_shop__stores.sql
@@ -10,16 +10,18 @@ renamed as (
 
     select
         id as store_id,
-        short as short_store_name,
+        short as store_short_name,
         name as store_name,
         address_id as store_address_id,
-        phone_number as store_phone_nb,
-        created_at as store_inception_at,
+        phone_number as store_phone_number,
+        store_image,
+        created_at as store_created_at,
         updated_at as store_updated_at,
         hidden as is_store_hidden,
-        ecom_inv_hidden as is_ecom_inv_hidden
+        ecom_inv_hidden as is_store_ecom_inv_hidden
 
     from source
+
 )
 
 select * from renamed
