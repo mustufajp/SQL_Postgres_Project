9f82473ae1d544552251321c87ac038920e2476d
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-01-10 07:51:42 +0000

    fixed sales

--- models/staging/saad_shop/stg_saad_shop__sales.sql
+++ models/staging/saad_shop/stg_saad_shop__sales.sql
@@ -19,14 +19,16 @@ renamed as (
         points_used,
         total_discount,
         estimated_age AS customer_age,
-        gender AS customer_gender,
+        gender AS customer_category,
         COALESCE(REPLACE((methods->0->>'type')::text, '"', ''), '') AS customer_payment_type,
         COALESCE(REPLACE((methods->0->>'method')::text, '"', ''), '') AS customer_payment_method,
         created_at AS sales_date,
         affiliate_commission_point,
         affiliate_commission_amount,
         (custom_discount->>'price')::numeric AS custom_discount_price,
-        (custom_discount->>'coupon')::numeric AS custom_discount_coupon
+        (custom_discount->>'coupon')::numeric AS custom_discount_coupon,
+        custom_point_back
+
     from source
 )
 
