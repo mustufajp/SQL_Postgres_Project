1dbabe94b90dfee38bfd2cecab224f0a26710dae
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-01-10 15:38:33 +0000

    fixed sales table

--- models/intermediate/finance/int_refunds_removed_from_sales_transaction.sql
+++ models/intermediate/finance/int_refunds_removed_from_sales_transaction.sql
@@ -25,5 +25,33 @@ int_refunds_removed_from_sales_transaction as
 )
 
 SELECT 
-*
+--date
+sales_date,
+
+--id
+transaction_id,
+sales_id,
+sender_id as customer_id,
+employee_id,
+sales_store_id,
+
+--transaction detail
+case when is_refund=0 then sales_amount else 0 end as sales_amount,
+case when is_refund=0 then points_given else 0 end as points_given,
+case when is_refund=0 then points_used else 0 end as points_used,
+case when is_refund=0 then total_discount else 0 end as total_discount,
+case when is_refund=0 then affiliate_commission_point else 0 end as affiliate_commission_point,
+case when is_refund=0 then affiliate_commission_amount else 0 end as affiliate_commission_amount,
+case when is_refund=0 then custom_discount_price else 0 end as custom_discount_price,
+case when is_refund=0 then custom_discount_coupon else 0 end as custom_discount_coupon,
+transaction_real_world_amount,
+transaction_type,
+is_refund,
+
+--customer info
+customer_age,
+customer_category,
+customer_payment_type,
+customer_payment_method
+
 from int_refunds_removed_from_sales_transaction
