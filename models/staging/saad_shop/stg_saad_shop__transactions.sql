c0749cdd08004aa87f47d57d1956238ce2879aa4
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-01-10 06:59:29 +0000

    renamed and included columns from raw data to conform with dbt best practices

--- models/staging/saad_shop/stg_saad_shop__transactions.sql
+++ models/staging/saad_shop/stg_saad_shop__transactions.sql
@@ -9,14 +9,16 @@ source as (
 renamed as (
 
     select
-        id as transaction_id
-        ,amount as transaction_amount
-        ,point_amount as transaction_point_amount
-        ,real_world_amount as transaction_real_world_amount
-        ,sender_id
-        ,created_at as transaction_date
-        ,type as transaction_type
-        ,parent_transaction_id
+        id as transaction_id,
+        amount as transaction_amount,
+        point_amount as transaction_point_amount,
+        real_world_amount as transaction_real_world_amount,
+        sender_id,
+        receiver_id,
+        created_at as transaction_at,
+        type as transaction_type,
+        status as transaction_status,
+        parent_transaction_id
 
     from source
 
