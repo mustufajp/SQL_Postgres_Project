1ca7b7f57aeab603113c77866fa0932575c3ab67
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-01-12 09:49:32 +0000

    removed some of the unnecessary columns

--- models/marts/dashboard/looker_dashboard/customer_analysis_dashboard.sql
+++ models/marts/dashboard/looker_dashboard/customer_analysis_dashboard.sql
@@ -1,65 +1,22 @@
 with 
-sales as 
+customer_analysis_dashboard as 
 (
     select 
-    {{ dbt_utils.star(from=ref('int_added_churn_rate'), except=["year_month"]) }}
-    from {{ ref('int_added_churn_rate') }}
+    {{ dbt_utils.star(from=ref('int_joined_sales_emolyee_customer_store_info'), except=[
+        "store_id",
+        "employee_id",
+        "sales_id",
+        "affiliate_commission_point",
+        "affiliate_commission_amount",
+        "custom_discount_price",
+        "custom_discount_coupon",
+        "transaction_real_world_amount",
+        "transaction_type",
+        "points_given"
+        
+        ]) }}
+    from {{ ref('int_joined_sales_emolyee_customer_store_info') }}
 ),
-
-employee_info as (
-    select
-    --joining key
-    employee_id,
-    --info to include
-    employee_type,
-    employee_status,
-    employee_first_name,
-    employee_last_name
-    from {{ ref('int_add_info_to_employee') }}
-),
-
-customer_info as (
-    select
-    --joining key
-    customer_id,
-    --info to include
-    customer_phone_number,
-    is_customer_email_verified,
-    customer_email,
-    is_magazine_subscribed,
-    is_agree_to_term_of_service,
-    customer_first_name,
-    customer_last_name,
-    customer_gender,
-    customer_date_of_birth,
-    customer_post_code,
-    customer_prefecture,
-    customer_city,
-    customer_combined_address
-    from {{ ref('int_add_addresses_to_user_info') }}
-),
-
-store_info as (
-    select 
-        --joining key
-        store_id,
-
-        --information to include
-        store_short_name,
-        store_post_code,
-        store_prefecture,
-        store_city,
-        store_combined_address
-    from {{ ref('int_address_joined_to_store') }}
-),
-
-customer_analysis_dashboard as (
-    select *
-    from sales
-    left join customer_info using (customer_id)
-    left join employee_info using (employee_id)
-    left join store_info using (store_id)
-    )
 
 select 
 *
