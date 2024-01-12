8f8847ad08da6a956ea8088c6d482720b6d41981
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-01-12 17:09:06 +0000

    changed

--- models/intermediate/finance/sales/int_churn_analysis.sql
+++ models/intermediate/finance/sales/int_churn_analysis.sql
@@ -10,7 +10,7 @@ monthly_churn as (
     select
     date_trunc('month', sales_date) as year_month,
     count(distinct customer_id) as distinct_customers,
-    count (distinct case when date_trunc('month', sales_date)=date_trunc('month', last_purchase) then customer_id end) as last_purchase_customers,
+    count (distinct case when date_trunc('month', sales_date)=date_trunc('month', last_purchase) then customer_id end) as last_purchase_customers
     from sales
     group by date_trunc('month', sales_date)
 ),
