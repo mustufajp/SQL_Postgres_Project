5f4ebebdaffd59499a5e613942ffcfce7820d724
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-01-15 07:29:44 +0000

    removed unnecessary columns

--- models/marts/dashboard/looker_dashboard/churn_analysis.sql
+++ models/marts/dashboard/looker_dashboard/churn_analysis.sql
@@ -1,9 +1,15 @@
 with 
 
 churn_rate as (
-    select *
-    from {{ ref('int_churn_analysis') }}
+    select 
+
+    {{ dbt_utils.star(from=ref('int_churn_analysis_segment'), except=[
+        "distinct_customers",
+        "churned_customers", 
+        ]) }}
+    from {{ ref('int_churn_analysis_segment') }}
 )
 
 select *
 from churn_rate
+
