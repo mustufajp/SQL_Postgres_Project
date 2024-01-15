d4db7b5fed12dd390572a66031b833b51337d1c4
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-01-15 07:54:12 +0000

    added churn rate to main sales data

--- models/marts/dashboard/looker_dashboard/customer_analysis_dashboard.sql
+++ models/marts/dashboard/looker_dashboard/customer_analysis_dashboard.sql
@@ -2,7 +2,7 @@ with
 customer_analysis_dashboard as 
 (
     select 
-    {{ dbt_utils.star(from=ref('int_joined_sales_emolyee_customer_store_info'), except=[
+    {{ dbt_utils.star(from=ref('int_churn_analysis_added_to_sales'), except=[
         "store_id",
         "employee_id",
         "sales_id",
@@ -12,10 +12,10 @@ customer_analysis_dashboard as
         "custom_discount_coupon",
         "transaction_real_world_amount",
         "transaction_type",
-        "points_given"
-        
+        "points_given",
+        "year_month"
         ]) }}
-    from {{ ref('int_joined_sales_emolyee_customer_store_info') }}
+    from {{ ref('int_churn_analysis_added_to_sales') }}
 )
 
 select 
