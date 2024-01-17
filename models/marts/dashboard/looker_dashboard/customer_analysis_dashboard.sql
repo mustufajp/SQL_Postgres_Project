d2eae69b051811d2ff167a999a558f03a76825c5
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-01-17 15:13:59 +0000

    included birthday and days from last purchase

--- models/marts/dashboard/looker_dashboard/customer_analysis_dashboard.sql
+++ models/marts/dashboard/looker_dashboard/customer_analysis_dashboard.sql
@@ -14,7 +14,13 @@ customer_analysis_dashboard as
         "transaction_type",
         "points_given",
         "year_month"
-        ]) }}
+        ]) }},
+    DATE_PART('day', CURRENT_DATE AT TIME ZONE 'Asia/Tokyo'-sales_date) as days_last_purchase,
+    CASE 
+        WHEN EXTRACT(DOY FROM customer_date_of_birth) >= EXTRACT(DOY FROM CURRENT_DATE AT TIME ZONE 'Asia/Tokyo')
+        THEN EXTRACT(DOY FROM customer_date_of_birth) - EXTRACT(DOY FROM CURRENT_DATE AT TIME ZONE 'Asia/Tokyo')
+        ELSE EXTRACT(DOY FROM customer_date_of_birth + INTERVAL '1 year') - EXTRACT(DOY FROM CURRENT_DATE AT TIME ZONE 'Asia/Tokyo')
+    END AS days_until_birthday
     from {{ ref('int_churn_analysis_added_to_sales') }}
 )
 
