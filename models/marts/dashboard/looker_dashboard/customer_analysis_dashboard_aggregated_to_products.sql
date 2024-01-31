ab73020218df187d467f878799894027e251ed87
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-01-31 16:22:13 +0000

    reduced the product file and combined the mart sales and int product

--- models/marts/dashboard/looker_dashboard/customer_analysis_dashboard_aggregated_to_products.sql
+++ models/marts/dashboard/looker_dashboard/customer_analysis_dashboard_aggregated_to_products.sql
@@ -7,30 +7,14 @@ product as (
 ),
 sales as (
     select 
-    sales_date,
-    transaction_id,
-    customer_id,
-    customer_first_name,
-    customer_last_name,
-    customer_age,
-    customer_category,
-    customer_payment_method,
-    is_customer_email_verified,
-    is_magazine_subscribed,
-    is_agree_to_term_of_service,
-    customer_gender,
-    customer_date_of_birth,
-    customer_prefecture,
-    customer_combined_address,
-    employee_type,
-    employee_status,
-    employee_first_name,
-    employee_last_name,
-    store_short_name,
-    store_prefecture,
-    store_combined_address
-    from {{ ref('int_joined_sales_emolyee_customer_store_info') }}
-),
+     {{ dbt_utils.star(from=ref('customer_analysis_dashboard'), except=[
+        "sales_amount",
+        "points_used",
+        "total_discount"
+        ]) }}
+
+    from {{ ref('customer_analysis_dashboard') }}
+    ),
 
 customer_analysis_dashboard_aggregated_to_products as (
     select *
