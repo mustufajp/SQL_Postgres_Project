5e96aec8b7e33b95fb5a2065bd0a6e66fdec50b8
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-02-04 10:14:06 +0000

    fixed the except condition

--- models/marts/dashboard/looker_dashboard/customer_analysis_dashboard.sql
+++ models/marts/dashboard/looker_dashboard/customer_analysis_dashboard.sql
@@ -6,12 +6,6 @@ customer_analysis_dashboard as
         "store_id",
         "employee_id",
         "sales_id",
-        "affiliate_commission_point",
-        "affiliate_commission_amount",
-        "custom_discount_price",
-        "custom_discount_coupon",
-        "transaction_real_world_amount",
-        "transaction_type",
         "points_given",
         "year_month"
         ]) }},
