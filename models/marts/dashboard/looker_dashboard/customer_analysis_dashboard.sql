709b91b7f7441187efb91a9f563737a527c2ef01
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-02-05 06:38:57 +0000

    included english version of member status

--- models/marts/dashboard/looker_dashboard/customer_analysis_dashboard.sql
+++ models/marts/dashboard/looker_dashboard/customer_analysis_dashboard.sql
@@ -26,10 +26,18 @@ customer_analysis_dashboard as
     when customer_id is not null and sales_date=first_purchase then '新規会員'
     when customer_id is not null then '既存会員'
     when customer_id is null then '非会員'
-    end as member_status
+    end as member_status,
+    case 
+    when customer_id is not null and sales_date=first_purchase then 'New User'
+    when customer_id is not null then 'Existing User'
+    when customer_id is null then 'Non-User'
+    end as member_status_en
+
     from {{ ref('int_churn_analysis_added_to_sales') }}
 )
 
 select 
-*
+customer_category,
+count(transaction_id)
 from customer_analysis_dashboard
+group by customer_category
