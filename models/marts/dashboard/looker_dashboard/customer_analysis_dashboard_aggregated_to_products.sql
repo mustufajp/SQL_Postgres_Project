6272d3c1066d8fb8ca76512a0be378a67c05ee43
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-02-15 04:26:08 +0000

    changed the location of gift function

--- models/marts/dashboard/looker_dashboard/customer_analysis_dashboard_aggregated_to_products.sql
+++ models/marts/dashboard/looker_dashboard/customer_analysis_dashboard_aggregated_to_products.sql
@@ -9,16 +9,7 @@ product as (
 customer_analysis_dashboard_aggregated_to_products as (
     select 
     t.*,
-    concat(t.product_category,t.product_code,product_type) as sku_code_na_size,
-    CASE 
-        WHEN EXISTS (
-            SELECT 1 
-            FROM product sub
-            WHERE sub.transaction_id = t.transaction_id
-            AND sub.product_category ILIKE '%box%'
-        ) THEN 'ギフト'
-        ELSE '非ギフト'
-    END AS is_gift
+    concat(t.product_category,t.product_code,product_type) as sku_code_na_size
     from product t
 )
 
