afc44b2ab8b7fd8ef137fae20d09173c3104f78b
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-02-16 08:39:20 +0000

    removed customer info from the sales as they are irrelevant and create duplications

--- models/intermediate/finance/sales/int_joined_sales_emolyee_customer_store_info.sql
+++ models/intermediate/finance/sales/int_joined_sales_emolyee_customer_store_info.sql
@@ -18,27 +18,6 @@ employee_info as (
     from {{ ref('int_add_info_to_employee') }}
 ),
 
-customer_info as (
-    select
-    --joining key
-    customer_id,
-    --info to include
-    customer_phone_number,
-    is_customer_email_verified,
-    customer_email,
-    is_magazine_subscribed,
-    is_agree_to_term_of_service,
-    customer_first_name,
-    customer_last_name,
-    customer_gender,
-    customer_date_of_birth,
-    customer_post_code,
-    customer_prefecture,
-    customer_city,
-    customer_combined_address
-    from {{ ref('int_add_addresses_to_user_info') }}
-),
-
 store_info as (
     select 
         --joining key
@@ -57,7 +36,6 @@ store_info as (
  int_joined_sales_emolyee_customer_store_info as (
     select *
     from sales
-    left join customer_info using (customer_id)
     left join employee_info using (employee_id)
     left join store_info using (store_id)
     )
