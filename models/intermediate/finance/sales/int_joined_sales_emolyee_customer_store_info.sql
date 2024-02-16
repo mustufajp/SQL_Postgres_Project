b1767e552aaf1e94ee060925d77eb800d9731aff
Mustufa Mohammad Iwamoto <mustufajp@gmail.com>
2024-02-16 09:08:35 +0000

    reversed the previous decision and include last and first purchase as it can be relevant.

--- models/intermediate/finance/sales/int_joined_sales_emolyee_customer_store_info.sql
+++ models/intermediate/finance/sales/int_joined_sales_emolyee_customer_store_info.sql
@@ -3,33 +3,42 @@ sales as
 (
     select 
     *
-    from {{ ref('int_added_first_purchase_date') }}
+    {{ ref('int_added_first_purchase_date') }}
 ),
 
 employee_info as (
     select
-    --joining key
-    employee_id,
-    --info to include
-    employee_type,
-    employee_status,
-    employee_first_name,
-    employee_last_name
+       {{ dbt_utils.star(from=ref('int_add_info_to_employee'), except=[
+        "employee_store_id",
+        "employee_phone_number",
+        "employee_email",
+        "employee_gender",
+        "employee_date_of_birth",
+        "employee_profile_updated_at"
+        ]) }}
     from {{ ref('int_add_info_to_employee') }}
 ),
 
 store_info as (
     select 
-        --joining key
-        store_id,
-
-        --information to include
-        is_store_hidden,
-        store_short_name,
-        store_post_code,
-        store_prefecture,
-        store_city,
-        store_combined_address
+         {{ dbt_utils.star(from=ref('int_address_joined_to_store'), except=[
+        "store_name",
+        "store_address_id",
+        "store_phone_number",
+        "store_created_at",
+        "store_updated_at",
+        "is_store_ecom_inv_hidden",
+        "address_id",
+        "store_post_code",
+        "store_city",
+        "store_town",
+        "store_line_one",
+        "store_line_two",
+        "store_latitude",
+        "store_longtitude",
+        "store_country",
+        "store_combined_address"
+        ]) }}
     from {{ ref('int_address_joined_to_store') }}
 ),
 
